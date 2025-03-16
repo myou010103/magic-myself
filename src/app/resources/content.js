42ca24f645e42cbd8fb2459bcc75b7cf240a0b95
myou010103 <141080363+myou010103@users.noreply.github.com>
2025-03-16 16:01:37 +0800

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -6,10 +6,10 @@ const person = {
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
-  role: "Design Engineer",
+  role: "Day Dreamer",
   avatar: "/images/avatar.jpg",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  location: "Asia/Suzhou", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Chinese"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter = {
